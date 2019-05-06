23836dba8ec4d4e31f568d745ebdfc8689a07421
Lokesh Nambiar <lokesh.nambiar@gmail.com>
2019-05-06 13:09:28 +0530

    Initial Change

--- ElevatorProgram/Program.cs
+++ ElevatorProgram/Program.cs
@@ -19,7 +19,7 @@ namespace ElevatorProgram
             Building building = new Building(NO_OF_FLOORS, NO_OF_ELEVATORS, TRAVEL_TIME_BETWEEN_FLOOR);
             building.StartElevators();
             Floors = building.Floors;
-            //4 th floor Elevator button pressed
+            //Elevator button of a floor pressed
             Floors[3].PressElevatorButton(3);
         }
     }
